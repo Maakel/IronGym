cdfe0f6dd4175bdc3d9a5bce79553e696a63d1b5
Tim Karlsson <tim_tub@hotmail.com>
2017-02-08 21:48:46 +0100

    Testar onFocusChangeListener för EditText

--- app/src/main/java/com/example/hokku/irongym/gymprogram_a.java
+++ app/src/main/java/com/example/hokku/irongym/gymprogram_a.java
@@ -2,12 +2,42 @@ package com.example.hokku.irongym;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class gymprogram_a extends AppCompatActivity {
+
+    String sKnabojSet = "1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gymprogram_a);
+
+
+        final EditText etKnabojSet  = (EditText) findViewById(R.id.knabojSet);
+        final EditText etKnabojVikt       = (EditText) findViewById(R.id.knabojVikt);
+        final EditText etKnabojReps       = (EditText) findViewById(R.id.knabojReps);
+
+        final String sKnabojRepsHint = etKnabojReps.getHint().toString();
+
+        etKnabojSet.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                if (!hasFocus) {
+                    sKnabojSet = etKnabojSet.getText().toString();
+                    if (sKnabojSet.length() > 0) {
+                        etKnabojReps.setHint(sKnabojSet);
+                    } else {
+                        etKnabojReps.setHint(sKnabojRepsHint);
+                    }
+
+                }
+            }
+
+        });
+
     }
+
+
 }
